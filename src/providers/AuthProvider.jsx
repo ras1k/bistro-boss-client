28bf701a2785f4b94f6c23b068b2b6156f7ee00b
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-28 18:20:48 +0600

    jwt added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
@@ -39,12 +40,21 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            setLoading(false)
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data => {
+                    console.log(data.data.token)
+                    localStorage.setItem('access-token', data.data.token)
+                });
+            } else {
+                localStorage.removeItem('access-token');
+            };
+            setLoading(false);
         });
         return () => {
             return unsubscribe();
         }
-    }, [])
+    }, []);
 
     const authInfo = {
         user,

ce736ec1c1a159fbb88a4b8542eae1b2a48a65e9
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-11 15:29:30 +0600

    photourl added to server

--- src/pages/DashBoard/AddItem/AddItem.jsx
+++ src/pages/DashBoard/AddItem/AddItem.jsx
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form';
 const img_hosting_token = import.meta.env.VITE_Image_Upload_Token;
 const AddItem = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const img_hosting_URl = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+    const img_hosting_URl = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
     const onSubmit = data => {
         console.log(data)
         const fromData = new FormData();
@@ -16,10 +16,16 @@ const AddItem = () => {
             method: 'POST',
             body: fromData
         })
-        .then(res => res.json())
-        .then(imgResponse => {
-            console.log(imgResponse)
-        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                console.log(imgResponse)
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    const {name, category, price, recipe} = data;
+                    const newItem= {name, category, price: parseFloat(price), recipe, image: imgURL};
+                    console.log(newItem)
+                }
+            })
     };
     // console.log(errors);
     return (
@@ -39,45 +45,45 @@ const AddItem = () => {
                             className="input input-bordered w-full  " />
                     </div>
                     <div className='flex gap-5'>
-                    <div className="form-control w-full  ">
-                        <label className="label">
-                            <span className="label-text">Category<span className='text-red-600'>*</span></span>
-                        </label>
-                        <select defaultValue="Pick One" className="select select-bordered" {...register("category", { required: true })}>
-                            <option disabled>Pick one</option>
-                            <option>Pizza</option>
-                            <option>Soup</option>
-                            <option>Salad</option>
-                            <option>Dessert</option>
-                            <option>Drinks</option>
-                        </select>
+                        <div className="form-control w-full  ">
+                            <label className="label">
+                                <span className="label-text">Category<span className='text-red-600'>*</span></span>
+                            </label>
+                            <select defaultValue="Pick One" className="select select-bordered" {...register("category", { required: true })}>
+                                <option disabled>Pick one</option>
+                                <option>Pizza</option>
+                                <option>Soup</option>
+                                <option>Salad</option>
+                                <option>Dessert</option>
+                                <option>Drinks</option>
+                            </select>
+                        </div>
+                        <div className="form-control w-full  ">
+                            <label className="label">
+                                <span className="label-text font-semibold">Price<span className='text-red-600'>*</span></span>
+                            </label>
+                            <input type="number" placeholder="Price" {...register("price", { required: true })} className="input input-bordered w-full  " />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Recipe Details<span className='text-red-600'>*</span></span>
+                            </label>
+                            <textarea className="textarea textarea-bordered h-24" {...register("recipe", { required: true })} placeholder="Recipe"></textarea>
+                        </div>
                     </div>
                     <div className="form-control w-full  ">
                         <label className="label">
-                            <span className="label-text font-semibold">Price<span className='text-red-600'>*</span></span>
+                            <span className="label-text">Item Image<span className='text-red-600'>*</span></span>
                         </label>
-                        <input type="number" placeholder="Price" {...register("price", { required: true })} className="input input-bordered w-full  " />
+                        <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full  " />
                     </div>
-                </div>
-                <div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Recipe Details<span className='text-red-600'>*</span></span>
-                        </label>
-                        <textarea className="textarea textarea-bordered h-24" {...register("recipe", { required: true })} placeholder="Recipe"></textarea>
+                    <div className='text-center'>
+                        <input type="submit" className='btn btn-outline w-full btn-primary border-b-4 mt-4' value="Submit" />
                     </div>
-                </div>
-                <div className="form-control w-full  ">
-                    <label className="label">
-                        <span className="label-text">Item Image<span className='text-red-600'>*</span></span>
-                    </label>
-                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full  " />
-                </div>
-                <div className='text-center'>
-                    <input type="submit" className='btn btn-outline w-full btn-primary border-b-4 mt-4' value="Submit" />
-                </div>
                 </form>
-                
+
             </div>
         </div>
     );

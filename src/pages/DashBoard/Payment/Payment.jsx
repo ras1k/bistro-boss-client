54984424851121c56132b73d317f08e3d096fe57
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-12 23:35:01 +0600

    pk added

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk)
 const Payment = () => {
     return (
         <div className='w-full h-full px-10 ms-10 mt-2'>
-            <SectionTitle subHeading={'Please Provide'} heading={'Payment'}></SectionTitle>
-            payment
+            <SectionTitle
+                subHeading={'Please Provide'}
+                heading={'Payment'}>
+            </SectionTitle>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
+
         </div>
     );
 };

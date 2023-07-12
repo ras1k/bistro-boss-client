f65128dc2558c364bb7ed8fe556970a90fdc9361
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-12 17:44:00 +0600

    stripe added

---
+++ src/pages/DashBoard/Payment/CheckoutForm.jsx
@@ -0,0 +1,47 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React from 'react';
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements){
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+        if(card === null){
+            return;
+        }
+    }
+    return (
+        <div className='w-full h-full px-10 ms-10 mt-2'>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default CheckoutForm;

d40bb6e2647e83162fa5328083d38ed2467de749
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-13 02:48:04 +0600

    card number added

--- src/pages/DashBoard/Payment/CheckoutForm.jsx
+++ src/pages/DashBoard/Payment/CheckoutForm.jsx
@@ -1,20 +1,34 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
 
         const card = elements.getElement(CardElement);
-        if(card === null){
+        if (card === null) {
             return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+        if (error) {
+            console.log(error);
+            setCardError(error.message)
+        }
+        else {
+            setCardError('')
+            console.log(paymentMethod)
         }
     }
     return (
@@ -36,10 +50,16 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button type="submit" disabled={!stripe}>
-                    Pay
-                </button>
+                {
+                    cardError && <p className='text-red-600 text-center mt-10'>{cardError}</p>
+                }
+                <div className='text-center'>
+                    <button type="submit" className='btn w-1/2 btn-outline border-1 border-b-4 mt-4' disabled={!stripe}>
+                        Pay
+                    </button>
+                </div>
             </form>
+
         </div>
     );
 };

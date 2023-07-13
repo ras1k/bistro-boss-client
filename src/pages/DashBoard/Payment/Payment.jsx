98c7726c39597cc012b1fd4aa2c79a2447a9e922
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-14 01:56:43 +0600

    array reduced

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -3,9 +3,13 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
+import useCart from '../../../hooks/useCart';
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk)
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div className='w-full h-full px-10 ms-10 mt-2'>
             <SectionTitle
@@ -13,7 +17,7 @@ const Payment = () => {
                 heading={'Payment'}>
             </SectionTitle>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm price={price}></CheckoutForm>
             </Elements>
 
         </div>
